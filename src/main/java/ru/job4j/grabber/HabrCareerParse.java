a7bb4aac7a620d01b197f3c44b0a811981b1eff1
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-06-19 00:49:43 +0300

    2.3. Загрузка деталей поста. [#285212  [#285212]]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 public class HabrCareerParse {
@@ -17,11 +16,12 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format(
             "%s/vacancies/java_developer?page=", SOURCE_LINK);
 
-    private static final int NUMBER_OF_PAGES = 5;
+    private static final int NUMBER_OF_PAGES = 1;
 
     public static void main(String[] args) throws IOException {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         for (int i = 1; i <= NUMBER_OF_PAGES; i++) {
+            HabrCareerParse habrCareerParse = new HabrCareerParse();
             Connection connection = Jsoup.connect(String.format("%s%s", PAGE_LINK, i));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
@@ -33,8 +33,20 @@ public class HabrCareerParse {
                 Element linkElement = titleElement.child(0);
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", date, vacancyName, link);
+                String description = null;
+                try {
+                    description = habrCareerParse.retrieveDescription(link);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("%s %s %s %s%n", date, vacancyName, link, description);
             });
         }
+    }
+
+    private String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        return document.select(".vacancy-description__text").text();
     }
 }
