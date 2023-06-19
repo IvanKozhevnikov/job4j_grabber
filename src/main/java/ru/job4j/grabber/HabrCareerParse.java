f71175b17f40d94351340ae81d7330a8b699d363
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-06-19 17:23:02 +0300

    2.4. HabrCareerParse [#285213  [#285213]]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,11 +5,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.Parse;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
@@ -18,11 +23,42 @@ public class HabrCareerParse {
 
     private static final int NUMBER_OF_PAGES = 5;
 
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
     public static void main(String[] args) throws IOException {
+        HabrCareerParse habrCareerParse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> habrListPost = habrCareerParse.list(PAGE_LINK);
+        for (Post post : habrListPost) {
+            System.out.printf("%d. %s %s"
+                            + "%nОписание вакансии:%n"
+                            + "%s%n"
+                            + "%s"
+                            + "%n------------------------------------------------------" +
+                            "----------------------------------------------------------" +
+                            "----------------------------------------------------------" +
+                            "----------------------------------------------------------%n",
+                    post.getId(), post.getTitle(), post.getLink(), post.getDescription(), post.getCreated());
+        }
+    }
+
+    private String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        return document.select(".vacancy-description__text").text();
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> postList = new ArrayList<>();
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         for (int i = 1; i <= NUMBER_OF_PAGES; i++) {
-            HabrCareerParse habrCareerParse = new HabrCareerParse();
-            Connection connection = Jsoup.connect(String.format("%s%s", PAGE_LINK, i));
+            DateTimeParser dateTimeParse = null;
+            HabrCareerParse habrCareerParse = new HabrCareerParse(dateTimeParse);
+            Connection connection = Jsoup.connect(String.format("%s%s", link, i));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
@@ -32,21 +68,18 @@ public class HabrCareerParse {
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element linkElement = titleElement.child(0);
                 String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String linkOfPage = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 String description = null;
                 try {
-                    description = habrCareerParse.retrieveDescription(link);
+                    description = habrCareerParse.retrieveDescription(linkOfPage);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                System.out.printf("%s %s %s %s%n", date, vacancyName, link, description);
+                Post vacancyPost = new Post(vacancyName, linkOfPage, description, date);
+                postList.add(vacancyPost);
+                vacancyPost.setId(postList.indexOf(vacancyPost));
             });
         }
-    }
-
-    private String retrieveDescription(String link) throws IOException {
-        Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
-        return document.select(".vacancy-description__text").text();
+        return postList;
     }
 }
--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -8,7 +8,14 @@ public class Post {
     private String title;
     private String link;
     private String description;
-    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created;
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
 
     public int getId() {
         return id;
